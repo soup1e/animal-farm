9269de588e7f2f2f5d4cc84e254d84eac2e0f428
Sam Adams <sam.adams7785@gmail.com>
2023-01-09 13:05:24 -0800

    undoing everything cuz main branch

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,21 +1,3 @@
-import './Animal.css';
-
-export default function Animal({ animal }) {
-  let audio = new Audio(`/animalnoises/${animal.type}.mp3`);
-
-  const animalNoise = () => {
-    audio.play();
-  };
-
-  return (
-    <>
-      <div className="animal" style={{ top: animal.top, left: animal.left }}>
-        <button onClick={animalNoise}>
-          <img src={`/animals/${animal.type}.svg`} />
-          <p className="name"> {animal.name} </p>
-          {animal.says}
-        </button>
-      </div>
-    </>
-  );
+export default function Animal() {
+  return <></>;
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import './Main.css';
-import { animals } from '../../data.js';
-import Animal from '../Animal/Animal.js';
-import background from '../../background.png';
 
 export default function Main() {
-  return (
-    <main style={{ background: `url(${background})` }}>
-      {animals.map((animal) => (
-        <Animal key={animal.name} animal={animal} />
-      ))}
-    </main>
-  );
+  return <main></main>;
 }
