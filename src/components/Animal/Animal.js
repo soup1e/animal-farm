a9081f9e17e91894d033cd842c83d204735c803b
Sam Adams <sam.adams7785@gmail.com>
2023-01-09 12:32:23 -0800

    looping and display animals

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,13 @@
-export default function Animal() {
-  return <></>;
+import './Animal.css';
+
+export default function Animal({ animal }) {
+  return (
+    <>
+      <div className="animal" style={{ top: animal.top, left: animal.left }}>
+        <img src={`/animals/${animal.type}.svg`} />
+        <p className="name"> {animal.name} </p>
+        {animal.says}
+      </div>
+    </>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Main.css';
+import { animals } from '../../data.js';
+import Animal from '../Animal/Animal.js';
+import background from '../../background.png';
 
 export default function Main() {
-  return <main></main>;
+  return (
+    <main style={{ background: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal key={animal.name} animal={animal} />
+      ))}
+    </main>
+  );
 }
